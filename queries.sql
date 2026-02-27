f16690253cf72f6dbe015063aea59d4eead56770
Hasnain Amir <hasnain-amir@outlook.com>
2026-02-27 06:58:02 +0000

    added insert clients (2)

--- queries.sql
+++ queries.sql
@@ -1,8 +1,18 @@
 /* INSERT SKILLS */
 INSERT INTO Skill (skill_name, skill_type) VALUES
-('Java', 'Backend'),
+('PHP', 'Backend'),
 ('Python', 'Backend'),
 ('JavaScript', 'Frontend'),
-('JUnit', 'Testing'),
+('Postman', 'Testing'),
 ('MySQL', 'Database'),
 ('Figma', 'Design');
+
+/* INSERT CLIENTS */
+INSERT INTO Client
+(org_name, first_name, last_name, email, address, contact_preference)
+VALUES
+('Something Solutions Ltd', 'Sarah', 'Khan', 'sarah.khan@somethingsolutions.co.uk',
+'12 Burlington St, Birmingham, B12 4AJ', 'email'),
+
+('Haaris Ali Welfare', 'Haaris', 'Ali', 'haaris.ali@haarisaliwelfare.co.uk',
+'31 Broad St, Reading, RG21 7ET', 'post');
