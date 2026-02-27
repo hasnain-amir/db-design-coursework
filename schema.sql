12184f0f7f51c06f63d17c2b220b8b3762bcd502
Hasnain Amir <hasnain-amir@outlook.com>
2026-02-27 06:18:58 +0000

    added client schema

--- schema.sql
+++ schema.sql
@@ -1,8 +1,18 @@
 /* SKILLS SCHEMA */
-CREATE TABLE skills (
+CREATE TABLE Skills (
     skill_id INT AUTO_INCREMENT PRIMARY KEY,
     skill_name VARCHAR(100) NOT NULL,
     skill_type VARCHAR(100) NOT NULL,
     UNIQUE (skill_name, skill_type)
 );
 
+/* CLIENT SCHEMA */
+CREATE TABLE Client (
+    client_id INT AUTO_INCREMENT PRIMARY KEY,
+    org_name VARCHAR(150),
+    first_name varchar(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    address VARCHAR(255),
+    contact_preference ENUM('post', 'email') NOT NULL
+);
